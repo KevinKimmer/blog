593cb519bb458ae4955040493b14cab77f480abc
Kevin <kimmerkevin@gmail.com>
2024-04-17 15:43:46 -0700

    sending html to browser

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 //create server method takes in a callback function.
 //call back function get called everytime a request comes into the server
 const server = http.createServer((req, res) => {
@@ -10,10 +11,16 @@ const server = http.createServer((req, res) => {
     //res.setHeader('Content-Type', 'text/plain');
     res.setHeader('Content-Type', 'text/html');
 
-    res.write('<head><link rel=:"stylesheet"  href="#"></head>');
-    res.write('<p>hola</p>');
-    res.write('<p>amigos</p>');
-    res.end();
+    const data = fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            //res.write(data);
+            res.end(data);
+        }
+    });
+
 });
 
 //enable the server to listen for requests
@@ -21,3 +28,4 @@ const server = http.createServer((req, res) => {
 server.listen(3000, 'localhost', () => {
     console.log('listening on port 3000');
 });
+
