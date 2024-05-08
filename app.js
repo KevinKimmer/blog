9d26f0019ea5c04adb1adf86132494fe1a61b208
kevin <kimmerkevin@gmail.com>
2024-05-08 01:17:38 -0700

    mvc model and express routes implemented

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const path = require("path")
 const express = require('express');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogRoutes = require('./routes/blogRoutes')
 //express app
 const app = express();
+
 
 // connect to mongodb
 const dbURI = "mongodb://kevin:GHrEjf4KTPQf7@192.168.1.3:27017/blogs?directConnection=true";
@@ -32,40 +33,11 @@ app.set('view engine','ejs');
 
 // middleware & static files
 app.use(express.static('public'));
-
-//mongoose and mango sanbox routes
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'new blog',
-        snippet: 'about my new blog',
-        body: 'more about my new blog'
-    });
-
-    blog.save()
-})
-
-app.use((req, res, next) => {
-    console.log('new request made:');
-    console.log('host: ', req.hostname);
-    console.log('path: ', req.path);
-    console.log('method: ', req.method);
-    next();
-});
-
-app.use((req, res, next) => {
-    console.log('In the next middleware');
-    
-    next();
-});
+app.use(express.urlencoded({ extended: true}));
 
 // express sends status code to browser, and sets header
 app.get('/', (req, res)=> {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds mushrooms', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'}
-    ];
-    res.render('index', { title: 'Home', blogs });
+    res.redirect('/blogs')
 });
 app.get('/about', (req, res)=> {
     res.render('about', { title: 'About'});
@@ -78,9 +50,9 @@ app.get('/about', (req, res)=> {
 
 // });
 
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new Blog'});
-});
+//blog routes
+app.use('/blogs', blogRoutes);
+
 
 //404, needs to be in the bottom
 app.use((req, res)=> {
